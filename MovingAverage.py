d17a9062e3f95df9c66ede5675d33ff19db1b5bd
Konrad22 <konrad-schwede@gmx.de>
2018-08-18 21:59:14 +0200

    Completed Moving Average with interpolating missing entries with linear progression

--- MovingAverage.py
+++ MovingAverage.py
@@ -1,3 +1,6 @@
+from typing import Any, Union
+
+
 class MovingAverage:
     def evenly_spaced(self, data: list, window):
 
@@ -16,11 +19,7 @@ class MovingAverage:
         ma = max(keys)
         filled_list = list()
 
-        for i in range(mi, ma+1):
-            if i in d:
-                current_key = i
-            current_element = d[current_key]
-            filled_list.append(current_element)
+        filled_list = self.create_complete_list(d, ma, filled_list, mi)
 
         evenly_spaced = self.evenly_spaced(filled_list, window)
         result = dict()
@@ -28,3 +27,31 @@ class MovingAverage:
             if key - window + 1 >= mi:
                 result[key] = evenly_spaced[key - mi - window + 1]
         return result
+
+    def create_complete_list(self, d, ma, filled_list, mi):
+        for i in range(mi, ma + 1):
+            if i in d:
+                current_key = i
+                current_element = d[current_key]
+                filled_list.append(current_element)
+            else:
+                overall_delta = 1
+                for j in range(i, ma + 1):
+                    if j not in d:
+                        overall_delta = overall_delta + 1
+                    else:
+                        filled_list = filled_list + self.fill_empty_between_two_points(d, i, overall_delta)
+                        break
+        return filled_list
+
+    def fill_empty_between_two_points(self, d, first_empty_position, overall_delta):
+        new_elements = list()
+        delta_now = 1
+        while delta_now < overall_delta:
+            current_value = self.interpolate(overall_delta, delta_now, d[first_empty_position - 1], d[first_empty_position + overall_delta - 1])
+            new_elements.append(current_value)
+            delta_now = delta_now + 1
+        return new_elements
+
+    def interpolate(self, overall_delta, delta_now, l, r):
+        return l + delta_now * (r - l) / overall_delta
--- test_movingAverage.py
+++ test_movingAverage.py
@@ -41,12 +41,85 @@ class TestMovingAverage(TestCase):
 
     def test_MovingDateAverageWith2DayEvenSpacedLength3(self):
         d = {1: 1, 3: 4, 5: 5}
-        expected = {3: (1+(1+4)/2+4) / 3, 5: (4+(4+5)/2+5) / 3}
+        expected = {3: (1 + (1 + 4) / 2 + 4) / 3, 5: (4 + (4 + 5) / 2 + 5) / 3}
         result = MovingAverage().arbitrarily_spaced(d, 3)
         self.assertEqual(expected, result)
 
     def test_MovingDateAverageWith2DayEvenSpacedLength4(self):
         d = {1: 1, 3: 4, 5: 5}
-        expected = {5: (1+(1+4)/2+4+(4+5)/2+5) / 5}
+        expected = {5: (1 + (1 + 4) / 2 + 4 + (4 + 5) / 2 + 5) / 5}
         result = MovingAverage().arbitrarily_spaced(d, 5)
         self.assertEqual(expected, result)
+
+    def test_interpolate(self):
+        l = 0
+        r = 1
+        overall_delta = 2
+        delta_now = 1
+        expected = 0.5
+        result = MovingAverage().interpolate(overall_delta, delta_now, l, r)
+        self.assertEqual(expected, result)
+
+    def test_interpolatemore(self):
+        l = 0
+        r = 1
+        overall_delta = 4
+        delta_now = 2
+        expected = 0.5
+        result = MovingAverage().interpolate(overall_delta, delta_now, l, r)
+        self.assertEqual(expected, result)
+
+    def test_interpolateleft(self):
+        l = 0
+        r = 1
+        overall_delta = 1
+        delta_now = 0
+        expected = 0
+        result = MovingAverage().interpolate(overall_delta, delta_now, l, r)
+        self.assertEqual(expected, result)
+
+    def test_interpolateright(self):
+        l = 1
+        r = 1
+        overall_delta = 1
+        delta_now = 0
+        expected = 1
+        result = MovingAverage().interpolate(overall_delta, delta_now, l, r)
+        self.assertEqual(expected, result)
+
+    def test_interpolatenegative(self):
+        l = 3
+        r = 1
+        overall_delta = 2
+        delta_now = 1
+        expected = 2
+        result = MovingAverage().interpolate(overall_delta, delta_now, l, r)
+        self.assertEqual(expected, result)
+
+    def test_fill_empty_entries(self):
+        d = {0: 5, 7: 10}
+        i = 1
+        overall_delta = 7
+        expected_length = 6
+        result = MovingAverage().fill_empty_between_two_points(d, i, overall_delta)
+        self.assertEqual(expected_length, len(result))
+
+    def test_fill_empty_entries_value(self):
+        d = {0: 0, 10: 10}
+        i = 1
+        overall_delta = 10
+        expected_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        result = MovingAverage().fill_empty_between_two_points(d, i, overall_delta)
+        self.assertListEqual(expected_list, result)
+
+    def test_create_complete_list_does_not_add_entries_if_already_full(self):
+        d = {0: 0, 1: 10}
+        filled_list = list()
+        result = MovingAverage().create_complete_list(d, 2, filled_list, 0)
+        self.assertListEqual(list(d.values()), result)
+
+    def test_create_complete_list_adds_one_entry(self):
+        d = {0: 0, 2: 10}
+        filled_list = list()
+        result = MovingAverage().create_complete_list(d, 3, filled_list, 0)
+        self.assertListEqual([0, 5, 10], result)
