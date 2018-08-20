c70c7bf85cf4a42c06dd41e23135529487e9183d
vcsjj <vcsjj@gmx.de>
2018-08-20 21:48:23 +0200

    fail one unit test (check CI response)

--- Tests/test_emaIndicator.py
+++ Tests/test_emaIndicator.py
@@ -8,6 +8,7 @@ class TestEmaIndicator(TestCase):
         alpha = 0.5
         s = [0, 10, 100]
         result = EmaIndicator().calculate_with_alpha(s, alpha)
+        self.fail()
         self.assertEqual(s[0], result[0])
         self.assertEqual(s[1], result[1])
         self.assertEqual(s[2], result[2] + 0.5 * result[1])
